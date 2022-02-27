8737b33be271197d4cc5e952452f344cd4e41445
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-02-27 14:45:21 +0300

    10.3 Encoding - UTF-16

---
+++ 10-3/main.cpp
@@ -0,0 +1,51 @@
+#include <QCoreApplication>
+#include <QFile>
+#include <QDir>
+#include <QTextStream>
+#include <QRandomGenerator>
+
+QString makeData()
+{
+    QString data;
+    data.append("Unicode test\r\n");
+
+    for (int i = 0; i < 10; i++)
+    {
+        // Not working on 6.2.3!!!
+        // It is UTF-16, not UTF-8
+        int number = QRandomGenerator::global()->bounded(65535); // 1112063
+        data.append(QChar(number));
+        data.append("\r\n");
+    }
+
+    return data;
+}
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QString data = makeData();
+    qInfo() << data;
+
+    QFile file("data.txt");
+    if(file.open(QIODevice::WriteOnly))
+    {
+        QTextStream stream(&file);
+        stream.setEncoding(QStringConverter::Utf16);
+
+        stream << data;
+        stream.flush();
+
+        file.close();
+    }
+
+
+    qInfo() << "Done";
+    qInfo() << "UTF-16: " << data.utf16();
+    qInfo() << "ASCII: " << data.toLatin1();
+
+
+    return a.exec();
+}
