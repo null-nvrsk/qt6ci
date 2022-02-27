7fa38ce9a7d2dab01b22856ee98ed0ba569ccafe
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-02-27 14:47:32 +0300

    10.4 Encoding - Base64

---
+++ 10-4/main.cpp
@@ -0,0 +1,28 @@
+#include <QCoreApplication>
+
+QString makeData()
+{
+    QString data;
+    for(int i = 0; i < 10; i++)
+    {
+        data.append("Hello\r\n");
+    }
+    return data;
+}
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QString data = makeData();
+    QByteArray bytes(data.toUtf8());
+    QByteArray encoded = bytes.toBase64();
+    QByteArray decoded = QByteArray::fromBase64(encoded);
+
+    qInfo() << "Encoded: " << encoded;
+    qInfo() << "--------------";
+    qInfo() << "Decoded: " << decoded;
+
+    return a.exec();
+}
