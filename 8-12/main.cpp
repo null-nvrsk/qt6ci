45894f9536ceb42dd5a527441e05fb07c3984eb6
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-02-27 11:50:21 +0300

    8.12 QTextStream

---
+++ 8-12/main.cpp
@@ -0,0 +1,70 @@
+#include <QCoreApplication>
+#include <QFile>
+#include <QTextStream>
+
+void write(QFile &file)
+{
+    if(!file.isWritable())
+    {
+        qInfo() << "Unable to write to file!";
+        return;
+    }
+
+    QTextStream stream(&file);
+    stream.setEncoding(QStringConverter::LastEncoding);
+
+    stream.seek(file.size());
+    for(int i = 0; i < 5; i++)
+    {
+        stream << QString::number(i) << "-item\r\n";
+    }
+    stream.flush();
+
+    qInfo() << "File written";
+}
+
+void read(QFile &file)
+{
+    if(!file.isReadable())
+    {
+        qInfo() << "Unable to read from file!";
+        return;
+    }
+
+    QTextStream stream(&file);
+    stream.seek(0);
+    while (!stream.atEnd())
+    {
+        qInfo() << stream.readLine();
+    }
+
+
+}
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QString filename = "test.txt";
+
+    QFile file(filename);
+    if(file.open(QIODevice::ReadWrite))
+    {
+        QTextStream stream(&file);
+        qInfo() << "Encoding: " << stream.encoding();
+
+
+        write(file);
+        read(file);
+
+        file.close();
+    }
+    else
+    {
+        qInfo() << file.errorString();
+    }
+
+
+    return a.exec();
+}
