5b94c51c8d904af9e5142d9574333b4b1cc1a9b8
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-02-27 09:40:06 +0300

    6.2 QByteArrayView

---
+++ 6-2/main.cpp
@@ -0,0 +1,33 @@
+#include <QCoreApplication>
+#include <QByteArray>
+#include <QByteArrayView>
+
+
+void display(QByteArrayView &view)
+{
+    qInfo() << view;
+    qInfo() << "Size" << view.size();
+    qInfo() << "Data" << view.constData();
+    qInfo() << "Slice" << view.sliced(3, 6);
+    qInfo() << "Contains" << view.contains("ello");
+    qInfo() << "At" << view.at(1);
+    qInfo() << "Index" << view[0];
+    qInfo() << "Indexof" << view.indexOf('e');
+
+    for(auto c : view)
+    {
+        qInfo() << c;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QByteArray bytes("Hello world how are you");
+    QByteArrayView view(bytes);
+
+    display(view);
+
+    return a.exec();
+}
