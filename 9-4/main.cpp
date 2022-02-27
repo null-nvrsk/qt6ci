fad20c7ef395324ee842cf86c02f1e2d6a68e7fd
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-02-27 14:37:40 +0300

    9.4 QLoggingCategory

---
+++ 9-4/main.cpp
@@ -0,0 +1,23 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QLoggingCategory>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QLoggingCategory logtest("testing");
+    logtest.setEnabled(QtMsgType::QtDebugMsg, false);
+
+    qDebug() << "Debug = " << logtest.isDebugEnabled();
+    qInfo() << "Info = " << logtest.isInfoEnabled();
+    qWarning() << "Warning = " << logtest.isWarningEnabled();
+    qCritical() << "Critical = " << logtest.isCriticalEnabled();
+
+    qDebug(logtest) << "This is a debug";
+    qInfo(logtest) << "This is a info";
+    qWarning(logtest) << "This is a warning";
+    qCritical(logtest) << "This is a critical";
+
+    return a.exec();
+}
