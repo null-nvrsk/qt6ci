333d02627968c8c66590edd8a0e6ddf8e08ddf98
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-02-27 14:49:30 +0300

    10.5 Encoding - Hex

---
+++ 10-5/main.cpp
@@ -0,0 +1,29 @@
+#include <QCoreApplication>
+
+QString makeData()
+{
+    QString data;
+    for(int i = 0; i < 10; i++)
+    {
+        data.append("Hello\r\n");
+    }
+    return data;
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QString data = makeData();
+    QByteArray bytes(data.toUtf8());
+
+    QByteArray encoded = bytes.toHex();
+    QByteArray decoded = QByteArray::fromHex(encoded);
+
+    qInfo() << "Encoded: " << encoded;
+    qInfo() << "--------------";
+    qInfo() << "Decoded: " << decoded;
+
+
+    return a.exec();
+}
