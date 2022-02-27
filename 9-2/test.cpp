12f90b2bd01af8fea8e62a0a99d4ebac9e6d9395
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-02-27 14:33:45 +0300

    9.2 Creating a logger class

---
+++ 9-2/test.cpp
@@ -0,0 +1,12 @@
+#include "test.h"
+
+Test::Test(QObject *parent)
+    : QObject{parent}
+{
+    qDebug() << this << "Constructed";
+}
+
+void Test::testing()
+{
+    qWarning() << "This is a test!";
+}
